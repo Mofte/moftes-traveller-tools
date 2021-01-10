36b5c89e30c5369b8cf394c8e6453f7b58013c31
Mofte <mail@mofte.net>
2021-01-10 14:54:11 +0100

    fix

--- uwp.py
+++ uwp.py
@@ -3,20 +3,20 @@
 
 divider = "\n" + "-"*48 + "\n"
 
-def translate_uwp(uwp):
-  uwp = uwp.replace("A", "10")
-  uwp = uwp.replace("a", "10")
-  uwp = uwp.replace("B", "11")
-  uwp = uwp.replace("b", "11")
-  uwp = uwp.replace("C", "12")
-  uwp = uwp.replace("c", "12")
-  uwp = uwp.replace("D", "13")
-  uwp = uwp.replace("d", "13")
-  uwp = uwp.replace("E", "14")
-  uwp = uwp.replace("e", "14")
-  uwp = uwp.replace("F", "15")
-  uwp = uwp.replace("f", "15")
-  return uwp
+def translate_code(code):
+  code = code.replace("A", "10")
+  code = code.replace("a", "10")
+  code = code.replace("B", "11")
+  code = code.replace("b", "11")
+  code = code.replace("C", "12")
+  code = code.replace("c", "12")
+  code = code.replace("D", "13")
+  code = code.replace("d", "13")
+  code = code.replace("E", "14")
+  code = code.replace("e", "14")
+  code = code.replace("F", "15")
+  code = code.replace("f", "15")
+  return code
 
 port = ["A - hervorragend","B - gut","C - mittelmäßig","D - schlecht","E - lediglich eine Grenzinstallation","X - kein Raumhafen"]
 port_out = "Raumhafenklasse: "
@@ -61,18 +61,23 @@ gov_in = input("6. Bitte den Regierungstyp (0-D) eingeben: ")
 just_in = input("7. Bitte den Justizgrad (0-9+) eingeben: ")
 tech_in = input("8. Bitte das Technologielevel (0-F) eingeben: ")
 
-uwp = port_in + size_in + atmo_in + hydro_in + pop_in + gov_in + just_in + tech_in
+translate_code(port_in)
+translate_code(size_in)
+translate_code(atmo_in)
+translate_code(hydro_in)
+translate_code(pop_in)
+translate_code(gov_in)
+translate_code(just_in)
+translate_code(tech_in)
 
-translate_uwp(uwp)
-
-port_out += port[int(uwp(0))]
-size_out += size[int(uwp(1))]
-atmo_out += atmo[int(uwp(2))]
-hydro_out += hydro[int(uwp(3))]
-pop_out += pop[int(uwp(4))]
-gov_out += gov[int(uwp(5))]
-just_out += just[int(uwp(6))]
-tech_out += tech[int(uwp(7))]
+port_out += port[int(port_in)]
+size_out += size[int(size_in)]
+atmo_out += atmo[int(atmo_in)]
+hydro_out += hydro[int(hydro_in)]
+pop_out += pop[int(pop_in)]
+gov_out += gov[int(gov_in)]
+just_out += just[int(just_in)]
+tech_out += tech[int(tech_in)]
 
 print(divider)
 
